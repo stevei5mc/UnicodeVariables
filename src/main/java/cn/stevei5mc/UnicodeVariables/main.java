88b6a05822caabdb03f7016bd10e67504cfd4f03
stevei5mc <2978383306@qq.com>
2023-11-22 21:02:18 +0800

    调整加载顺序

--- src/main/java/cn/stevei5mc/UnicodeVariables/main.java
+++ src/main/java/cn/stevei5mc/UnicodeVariables/main.java
@@ -32,31 +32,7 @@ import cn.stevei5mc.UnicodeVariables.ucodef8;
 public class main extends PluginBase {
     public void onEnable() {
         if (this.getServer().getPluginManager().getPlugin("Tips") != null) {
-            Api.registerVariables("ucodee0", ucodee0.class);
-            Api.registerVariables("ucodee1", ucodee1.class);
-            Api.registerVariables("ucodee2", ucodee2.class);
-            Api.registerVariables("ucodee3", ucodee3.class);
-            Api.registerVariables("ucodee4", ucodee4.class);
-            Api.registerVariables("ucodee5", ucodee5.class);
-            Api.registerVariables("ucodee6", ucodee6.class);
-            Api.registerVariables("ucodee7", ucodee7.class);
-            Api.registerVariables("ucodee8", ucodee8.class);
-            Api.registerVariables("ucodee9", ucodee9.class);
-            Api.registerVariables("ucodeea", ucodee0.class);
-            Api.registerVariables("ucodeeb", ucodee1.class);
-            Api.registerVariables("ucodeec", ucodee2.class);
-            Api.registerVariables("ucodeed", ucodee3.class);
-            Api.registerVariables("ucodeee", ucodee4.class);
-            Api.registerVariables("ucodeef", ucodee5.class);
-            Api.registerVariables("ucodef0", ucodef0.class);
-            Api.registerVariables("ucodef1", ucodef1.class);
-            Api.registerVariables("ucodef2", ucodef2.class);
-            Api.registerVariables("ucodef3", ucodef3.class);
-            Api.registerVariables("ucodef4", ucodef4.class);
-            Api.registerVariables("ucodef5", ucodef5.class);
-            Api.registerVariables("ucodef6", ucodef6.class);
-            Api.registerVariables("ucodef7", ucodef7.class);
-            Api.registerVariables("ucodef8", ucodef8.class);
+            this.LoadVariables();
             this.getLogger().info("UnicodeVariables 加载成功");
             this.getLogger().info("§a支持显示范围 §eglyph_E0 §a==>> §eglyph_F8");
             this.getLogger().warning("§c警告:");
@@ -72,5 +48,33 @@ public class main extends PluginBase {
     public void onDisable() {
         this.getLogger().info("UnicodeVariables 停止运行");
         this.getLogger().info("§6感谢你的使用");
+    }
+
+    public void LoadVariables() {
+        Api.registerVariables("ucodee0", ucodee0.class);
+        Api.registerVariables("ucodee1", ucodee1.class);
+        Api.registerVariables("ucodee2", ucodee2.class);
+        Api.registerVariables("ucodee3", ucodee3.class);
+        Api.registerVariables("ucodee4", ucodee4.class);
+        Api.registerVariables("ucodee5", ucodee5.class);
+        Api.registerVariables("ucodee6", ucodee6.class);
+        Api.registerVariables("ucodee7", ucodee7.class);
+        Api.registerVariables("ucodee8", ucodee8.class);
+        Api.registerVariables("ucodee9", ucodee9.class);
+        Api.registerVariables("ucodeea", ucodee0.class);
+        Api.registerVariables("ucodeeb", ucodee1.class);
+        Api.registerVariables("ucodeec", ucodee2.class);
+        Api.registerVariables("ucodeed", ucodee3.class);
+        Api.registerVariables("ucodeee", ucodee4.class);
+        Api.registerVariables("ucodeef", ucodee5.class);
+        Api.registerVariables("ucodef0", ucodef0.class);
+        Api.registerVariables("ucodef1", ucodef1.class);
+        Api.registerVariables("ucodef2", ucodef2.class);
+        Api.registerVariables("ucodef3", ucodef3.class);
+        Api.registerVariables("ucodef4", ucodef4.class);
+        Api.registerVariables("ucodef5", ucodef5.class);
+        Api.registerVariables("ucodef6", ucodef6.class);
+        Api.registerVariables("ucodef7", ucodef7.class);
+        Api.registerVariables("ucodef8", ucodef8.class);
     }
 }
